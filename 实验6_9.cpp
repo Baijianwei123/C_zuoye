58a2f6d0129cfa5cf4f7cf323a92664791bfe7a0
Baijianwei123 <31720066+Baijianwei123@users.noreply.github.com>
2017-11-11 22:34:58 +0800

    Add files via upload

---
+++ 实验6_9.cpp
@@ -0,0 +1,36 @@
+#include<iostream.h>
+void fun()
+{
+	int n,i=0,m;
+    cin>>m;
+
+		for(n=1;n<=m;n++)
+
+		{
+		if(n%7==0||n%11==0)
+
+		{   
+
+			i+=1;
+
+		    cout<<n<<"  "<<endl;
+
+		}
+
+	}
+
+	cout<<"i="<<i<<endl;
+
+}
+
+void main()
+
+{
+
+
+	void fun();
+
+	fun();
+
+}
+	
